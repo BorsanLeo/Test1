ab3f0257bfcceb065c04e1d62059848a0b62fa31
BorsanLeo <leonardo.borsan@gmail.com>
2025-05-12 01:50:38 +0200

    aggiustato #2

--- test1/src/test/java/test1/IntegerToRomanTest.java
+++ test1/src/test/java/test1/IntegerToRomanTest.java
@@ -2,3 +2,18 @@
 // NOME1 COGNOME1 MATRICOLA1 
 // NOME2 COGNOME2 MATRICOLA2 
 ////////////////////////////////////////////////////////////////////
+package test1;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class IntegerToRomanTest {
+    @Test
+    public void testConvert() {
+        assertEquals("I", IntegerToRoman.convert(1));
+        assertEquals("II", IntegerToRoman.convert(2));
+        assertEquals("III", IntegerToRoman.convert(3));
+    }
+
+}
